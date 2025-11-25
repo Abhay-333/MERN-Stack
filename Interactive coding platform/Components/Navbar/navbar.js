f01d9a58c7c7de6b14a7c803bdfa6f7809cd9ab3
Abhay-333 <abhayd0331@gmail.com>
2025-11-25 18:41:19 +0530

    Worked on interactive coding platform

--- Interactive coding platform/Components/Navbar/navbar.js
+++ Interactive coding platform/Components/Navbar/navbar.js
@@ -1,7 +1,19 @@
+
 class Navbar extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `
+    this.innerHTML = `<nav>
+      <div class="left">
+        <h1>Logo</h1>
+      </div>
+
+      <div class="right">
+        <a>Home</a>
+        <a>Practice</a>
+        <a>About</a>
+      </div>
+     
+    </nav>
         `;
-    customElements.define("Navbar", Navbar);
   }
 }
+customElements.define("custom-nav", Navbar);
