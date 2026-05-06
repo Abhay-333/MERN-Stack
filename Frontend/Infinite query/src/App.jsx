3f8a51e86bf90ba3928badac7b3129ecc609c2d7
Abhay-333 <abhayd0331@gmail.com>
2026-05-06 09:50:59 +0530

    done

--- Frontend/Infinite query/src/App.jsx
+++ Frontend/Infinite query/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { fetchProducts } from "./api/productApi";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import ProductCard from "./components/ProductCard";
 
 const App = () => {
-  const { data, isPending, error } = useQuery({
+  // Lazy Query = API call tab hoti hai jab tum manually trigger karte ho
+  //   Infinite Query: Control karta hai “data kaise load hoga (chunks/pages me)”
+  const { data, isPending, error } = useInfiniteQuery({
     queryKey: ["products"],
     queryFn: fetchProducts,
+    getNextPageParam: (lastPage, pages) => {
+      console.log(lastPage);
+      console.log(pages);
+      return pages + 1;
+    },
   });
   console.log(data);
 
@@ -14,7 +21,7 @@ const App = () => {
     return <h1 className="bg-zinc-800 text-white h-screen"> Loading...</h1>;
   return (
     <div className="bg-zinc-800 text-white flex flex-wrap gap-5 items-center justify-center">
-      {data.map((product) => (
+      {data?.map((product) => (
         <ProductCard product={product} key={product.id}></ProductCard>
       ))}
     </div>
--- Frontend/Infinite query/src/api/productApi.jsx
+++ Frontend/Infinite query/src/api/productApi.jsx
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-export const fetchProducts = async () => {
-  const products = await axios.get(`https://dummyjson.com/products`);
+export const fetchProducts = async ({ pageParam = 1 }) => {
+  let limit = 10;
+  let skip = (pageParam - 1) * limit;
+  const products = await axios.get(
+    `https://dummyjson.com/products?limit=${limit}&skip=${skip}`,
+  );
   return products.data.products;
 };
