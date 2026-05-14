aae679d5a70e84f712153a34efb81b73ffdb5c79
Abhay-333 <abhayd0331@gmail.com>
2026-05-14 20:04:25 +0530

    Learning Shandcn in nextjs

---
+++ Frontend/Nextjs/shadcn-ui/src/components/local/Navbar.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import React from "react";
+import { ThemeToggle } from "./ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-black text-white px-8 py-4 shadow-md">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">MyWebsite</h1>
+
+        <ul className="flex gap-8 text-lg">
+          <Link
+            href={"/"}
+            className="cursor-pointer hover:text-gray-400 transition"
+          >
+            Home
+          </Link>
+
+          <Link
+            href={"/about"}
+            className="cursor-pointer hover:text-gray-400 transition"
+          >
+            About
+          </Link>
+
+          <Link
+            href={"/contact"}
+            className="cursor-pointer hover:text-gray-400 transition"
+          >
+            Contact
+          </Link>
+
+          <ThemeToggle/>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
